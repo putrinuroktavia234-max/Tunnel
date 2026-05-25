a2746bce3abb53181d570b73d6fe5d976428dc6e
putrinuroktavia234-max <putrinuroktavia234@gmail.com>
2026-05-26 02:00:42 +0800

    add encrypted panel + installer

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo -e "\e[92m[*] Downloading installer...\e[0m"
+wget -qO /tmp/tunnel_run \
+  https://raw.githubusercontent.com/putrinuroktavia234-max/Tunnel/main/tunnel_run
+chmod +x /tmp/tunnel_run
+/tmp/tunnel_run
+rm -f /tmp/tunnel_run
