92ab7412c7269c2e8ee8a125495179d06503426e
putrinuroktavia234-max <putrinuroktavia234@gmail.com>
2026-05-27 03:14:46 +0800

    fix: heredoc installer

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -qO /tmp/tr "https://raw.githubusercontent.com/putrinuroktavia234-max/Tunnel/main/tunnel_run"
-chmod +x /tmp/tr && /tmp/tr; rm -f /tmp/tr
+wget -qO /tmp/tr https://raw.githubusercontent.com/putrinuroktavia234-max/Tunnel/main/tunnel_run
+chmod +x /tmp/tr && /tmp/tr
+rm -f /tmp/tr
