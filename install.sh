5aa043d9085acc3de6453b41654eac8725394a18
putrinuroktavia234-max <putrinuroktavia234@gmail.com>
2026-05-27 02:52:11 +0800

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget -qO /tmp/tunnel_run \
-  https://raw.githubusercontent.com/putrinuroktavia234-max/Tunnel/main/tunnel_run
-chmod +x /tmp/tunnel_run
-/tmp/tunnel_run
-rm -f /tmp/tunnel_run
